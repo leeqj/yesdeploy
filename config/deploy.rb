acfc61309d8f7e911680b1b02232cd5224f79181
deploy <deploy@main>
2015-01-12 04:00:56 +0000

    first commit

---
+++ config/deploy.rb
@@ -0,0 +1,75 @@
+# config valid only for current version of Capistrano
+lock '3.3.5'
+
+set :application, 'yes2'
+set :repo_url, 'http://svn.leho.com/svn/yes_bbc/trunk/'
+
+# Default branch is :master
+# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
+
+# Default deploy_to directory is /var/www/my_app_name
+ set :deploy_to, '/usr/share/nginx/www'
+
+# Default value for :scm is :git
+ set :scm, :svn
+
+# Default value for :format is :pretty
+ set :format, :pretty
+
+# Default value for :log_level is :debug
+ set :log_level, :debug
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# set :linked_files, fetch(:linked_files, []).push('config/database.yml')
+
+# Default value for linked_dirs is []
+# set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+ set :keep_releases, 5
+
+namespace :deploy do
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+       within release_path do
+         execute :rake, 'cache:clear'
+       end
+    end
+  end
+
+  task :updatecmd  do
+    #["config/database.yml", "config/config.yml"].each do |path|
+      #run "ln -fs #{shared_path}/#{path} #{release_path}/#{path}"
+    #end
+	on roles(:web) do
+	    execute "echo 'finish will exe update common' > ~/tttt.txt"
+#	    execute "/usr/share/nginx/www/current/app/base/cmd update > ~/update.txt"
+  	end
+  end
+	#task :updatecmd do 
+		#execute "echo 'finish will exe update common'" 
+	#	sh '/usr/share/nginx/www/current/app/base/cmd update'   
+	#end
+
+  #after :finishing do 
+	#invoke "deploy:updatecmd"
+  #	sh '/usr/share/nginx/www/current/app/base/cmd update'
+  #end
+    task :cmdupdate do
+	on roles(:web) do
+		execute "echo 1 >/home/vagrant/text.t"
+	end
+        #"echo 1 >/home/vagrant/text.t"
+  end
+	#after :finishing, :cmdupdate
+	#after  "deploy","deploy:updatecmd" 
+end
+after  "deploy","deploy:updatecmd" 
---
+++ config/deploy/staging.rb
@@ -0,0 +1,61 @@
+# Simple Role Syntax
+# ==================
+# Supports bulk-adding hosts to roles, the primary server in each group
+# is considered to be the first unless any hosts have the primary
+# property set.  Don't declare `role :all`, it's a meta role.
+
+#role :app, %w{vagrant@172.28.128.4 172.28.128.5}
+#role :web, %w{deploy@web1, web2}
+#role :db,  %w{deploy@example.com}
+
+
+# Extended Server Syntax
+# ======================
+# This can be used to drop a more detailed server definition into the
+# server list. The second argument is a, or duck-types, Hash and is
+# used to set extended properties on the server.
+
+server '172.28.128.4', 
+	user: 'vagrant', 
+	roles: %w{web}, 
+	ssh_options:{
+		user:'vagrant',
+		auth_methods: %w(publickey password),
+		password:'vagrant'
+	}
+server '172.28.128.5',
+        user: 'vagrant',
+        roles: %w{web},
+        ssh_options:{
+                user:'vagrant',
+                auth_methods: %w(publickey password),
+                password:'vagrant'
+        }
+#, my_property: :my_value
+set :deploy_to, '/usr/share/nginx/www'
+
+# Custom SSH Options
+# ==================
+# You may pass any option but keep in mind that net/ssh understands a
+# limited set of options, consult[net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start).
+#
+# Global options
+# --------------
+#  set :ssh_options, {
+#    keys: %w(/home/rlisowski/.ssh/id_rsa),
+#    forward_agent: false,
+#    auth_methods: %w(password)
+#  }
+#
+# And/or per server (overrides global)
+# ------------------------------------
+# server 'example.com',
+#   user: 'user_name',
+#   roles: %w{web app},
+#   ssh_options: {
+#     user: 'user_name', # overrides user setting above
+#     keys: %w(/home/user_name/.ssh/id_rsa),
+#     forward_agent: false,
+#     auth_methods: %w(publickey password)
+#     # password: 'please use keys'
+#   }
